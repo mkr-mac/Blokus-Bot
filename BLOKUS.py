a170c2b43b7d3ac2a8178ea4f11ce2ef157db334
Mickey McCargish <mccargishm@gmail.com>
2022-07-05 19:28:22 -0400

    Random AI and game loop in place

--- BLOKUS.py
+++ BLOKUS.py
@@ -1,6 +1,25 @@
 
+from ai import AI
+from board import Board
+
+
 def blokus():
-    pass
+
+    main_board = Board()
+    players = [AI(p) for p in range(4)]
+
+    moves_left = True
+
+    while moves_left:
+        moves_left = False
+        for p in players:
+            if p.has_valid_moves:
+                p.decide_action(main_board)
+                moves_left = True
+
+    print(main_board)    
+
+
 
 
 if (__name__ == "__main__"):
